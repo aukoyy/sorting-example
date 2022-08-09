91bb832394c25d948ba0aea2e44ecf4df0659071
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2022-08-09 23:17:14 +0200

    remove comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { compareAges, compareNames, getAge } from './Utils';
 
 const people = require('./data.json');
